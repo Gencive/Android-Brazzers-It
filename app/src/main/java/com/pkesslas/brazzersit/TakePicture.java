7f87ef037284fc7466c1956f941521a484d81613
Your Name <you@example.com>
2014-12-26 22:45:05 +0100

    Sélectionne une taille de photo adapter

--- app/src/main/java/com/pkesslas/brazzersit/TakePicture.java
+++ app/src/main/java/com/pkesslas/brazzersit/TakePicture.java
@@ -3,27 +3,26 @@ package com.pkesslas.brazzersit;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Camera;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 
-public class TakePicture extends ActionBarActivity implements View.OnClickListener, SurfaceHolder.Callback {
+public class TakePicture extends ActionBarActivity implements View.OnClickListener {
 	private Camera camera;
-	private SurfaceView preview;
-	private String currentPhotoPath;
+	private Preview preview;
+	private FrameLayout previewLayout;
 	private TextView takePictureButton;
 	private Context context;
 	private boolean cameraRelease = false;
@@ -41,14 +40,27 @@ public class TakePicture extends ActionBarActivity implements View.OnClickListen
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.take_picture);
 
-		preview = (SurfaceView) findViewById(R.id.preview);
-		preview.getHolder().addCallback(this);
-		preview.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		camera = Camera.open();
 
+		Camera.Parameters parameters = camera.getParameters();
+		Log.e("size", parameters.getPictureSize().height + " " + parameters.getPictureSize().width);
+		List<Camera.Size> list = parameters.getSupportedPictureSizes();
+		for (Camera.Size l : list) {
+			Log.e("elnlsgsgs", l.height + " " + l.width);
+			if (l.height == 720) {
+				parameters.setPictureSize(l.width, l.height);
+			//	break ;
+			}
+		}
+
+		camera.setParameters(parameters);
+
+		previewLayout = (FrameLayout) findViewById(R.id.preview);
+		preview = new Preview(this, camera);
 		context = this;
 		takePictureButton = (TextView) findViewById(R.id.btn_take_picture);
 		takePictureButton.setOnClickListener(this);
-		camera = Camera.open();
+		previewLayout.addView(preview);
 	}
 
 	@Override
@@ -73,77 +85,52 @@ public class TakePicture extends ActionBarActivity implements View.OnClickListen
 		}
 	}
 
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-		Camera.Parameters params = camera.getParameters();
-		List<Camera.Size> sizes = params.getSupportedPreviewSizes();
-		Camera.Size selected = sizes.get(0);
-		params.setPreviewSize(selected.width,selected.height);
-		camera.setParameters(params);
+	private static File getOutputMediaFile() {
+		File mediaStorageDir = new File("/sdcard/", "test/");
 
-		camera.setDisplayOrientation(90);
-		camera.startPreview();
-	}
-
-	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-		try {
-			camera.setDisplayOrientation(90);
-			camera.setPreviewDisplay(preview.getHolder());
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				return null;
+			}
 		}
-	}
 
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		Log.i("PREVIEW", "surfaceDestroyed");
-	}
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		File mediaFile;
+		mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + "test.png");
 
+		return mediaFile;
+	}
 
 	Camera.PictureCallback photoCallback=new Camera.PictureCallback() {
+		@Override
 		public void onPictureTaken(byte[] data, Camera camera) {
-			new SavePhotoTask().execute(data);
-			camera.startPreview();
+			//make a new picture file
+			File pictureFile = getOutputMediaFile();
+
+			if (pictureFile == null) {
+				return;
+			}
+			try {
+				FileOutputStream fos = new FileOutputStream(pictureFile);
+				fos.write(data);
+				fos.close();
+
+				displayPicture(pictureFile);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			preview.refreshCamera(camera);
 		}
 	};
 
-	class SavePhotoTask extends AsyncTask<byte[], String, String> {
-		private boolean result = false;
-		File photo;
-
-		@Override
-		protected String doInBackground(byte[]... jpeg) {
-			currentPhotoPath = getImagePath();
-			photo = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), currentPhotoPath);
-
-			if (photo.exists()) {
-				photo.delete();
-			}
-
-			try {
-				FileOutputStream fos = new FileOutputStream(photo.getPath());
-
-				fos.write(jpeg[0]);
-				fos.close();
-				Log.e("Photo path", photo.getAbsolutePath());
-
-				result = true;
-			} catch (java.io.IOException e) {
-				Log.e("PictureDemo", "Exception in photoCallback", e);
-			}
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(String string) {
-			if (result == true) {
-				cameraRelease = true;
-				camera.release();
-				Intent intent = new Intent(context, DisplayTakenPhoto.class);
-				intent.putExtra("path", photo.getAbsolutePath());
-				startActivity(intent);
-			}
-		}
+	protected void displayPicture(File photo) {
+		cameraRelease = true;
+		camera.release();
+		Intent intent = new Intent(context, DisplayTakenPhoto.class);
+		Log.i("Photo path", photo.getAbsolutePath());
+		intent.putExtra("path", photo.getAbsolutePath());
+		startActivity(intent);
 	}
 }
